069664e3184d2e749449021da30a2ddd5cb16af0
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-02-26 16:07:22 -0300

    Can add and edit posts

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -29,4 +29,29 @@ class PostsController extends AppController {
             $this->Session->setFlash(__('Unable to add your post.'));
         }
     }
+
+    public function edit($id = null) {
+	    if (!$id) {
+	        throw new NotFoundException(__('Invalid post'));
+	    }
+
+	    $post = $this->Post->findById($id);
+	    if (!$post) {
+	        throw new NotFoundException(__('Invalid post'));
+	    }
+
+	    // The tutorial had a bug here: CakeRequest::is doesn't work with an array.
+	    if ($this->request->is('post') || $this->request->is('put')) {
+	        $this->Post->id = $id;
+	        if ($this->Post->save($this->request->data)) {
+	            $this->Session->setFlash(__('Your post has been updated.'));
+	            return $this->redirect(array('action' => 'index'));
+	        }
+	        $this->Session->setFlash(__('Unable to update your post.'));
+	    }
+
+	    if (!$this->request->data) {
+	        $this->request->data = $post;
+	    }
+	}
 }
