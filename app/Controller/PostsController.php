8a376e216d5c3f6e46afd805bbf2c450bb89769b
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-02-26 14:18:10 -0300

    Halfway through adding posts

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,32 @@
+<?php
+class PostsController extends AppController {
+    public $helpers = array('Html', 'Form', 'Session');
+    public $components = array('Session');
+
+    public function index() {
+        $this->set('posts', $this->Post->find('all'));
+    }
+
+    public function view($id) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('post', $post);
+    }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Post->create();
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash(__('Your post has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Unable to add your post.'));
+        }
+    }
+}
