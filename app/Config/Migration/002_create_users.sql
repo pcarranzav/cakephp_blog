d9aaa0ec8fba492be118fc857e5496ab01f72119
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-02-26 16:44:58 -0300

    Add Migration folder to track schema changes, add users table

---
+++ app/Config/Migration/002_create_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(255),
+    role VARCHAR(20),
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL,
+    num_login_attempts INT UNSIGNED DEFAULT 0,
+    last_login_attempt DATETIME DEFAULT NULL
+);
